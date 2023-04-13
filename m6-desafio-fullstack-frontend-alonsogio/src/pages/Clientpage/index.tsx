3fb186fc742ffa4d9c87da981a12bc59a4938d81
GiovannaAlonso <106785638+Alonsogio@users.noreply.github.com>
2023-04-13 15:52:21 -0300

    fix page

--- m6-desafio-fullstack-frontend-alonsogio/src/pages/Clientpage/index.tsx
+++ m6-desafio-fullstack-frontend-alonsogio/src/pages/Clientpage/index.tsx
@@ -3,7 +3,6 @@ import { FontH1, FontH2, FontP } from "../../components/Fonts/fonts";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/ClientPageContext";
 import { AnyObject } from "yup";
-import { Navigate } from "react-router-dom";
 import { ModalCreate } from "../../components/Modal/modalCreate";
 import { ModalEdit } from "../../components/Modal/modalEdit";
 
@@ -19,44 +18,40 @@ export const Clientpage = () => {
         clientcont,
         renderModal,
         handleClick,
-        tokenClient,
         navigate
     } = useContext<AnyObject>(AuthContext);
 
     return (
         <>
-            {tokenClient ? (
-                <>
-                    <DivMain className="main">
-                        {renderModal}
-                        <HeaderStyled>
-                            <div onClick={() => exit()}>
-                                < LogoutIcon />
+            <DivMain className="main">
+                {renderModal}
+                <HeaderStyled>
+                    <div onClick={() => exit()}>
+                        < LogoutIcon />
+                    </div>
+                </HeaderStyled>
+                <body>
+                    <div className="welcome">
+                        <FontH1>Bem vindo(a), <FontP>{client.fullName}</FontP></FontH1>
+                    </div>
+                    <DivContainer className="DivContainer">
+                        <DivFontBtn className="divFontBtn">
+                            <div>
+                                <FontH2>Contacts</FontH2>
                             </div>
-                        </HeaderStyled>
-                        <body>
-                            <div className="welcome">
-                                <FontH1>Bem vindo(a), <FontP>{client.fullName}</FontP></FontH1>
+                            <div>
+                                <BtnCreate onClick={() =>
+                                    handleClick(<ModalCreate />)}>Create</BtnCreate>
                             </div>
-                            <DivContainer className="DivContainer">
-                                <DivFontBtn className="divFontBtn">
-                                    <div>
-                                        <FontH2>Contacts</FontH2>
-                                    </div>
-                                    <div>
-                                        <BtnCreate onClick={() =>
-                                            handleClick(<ModalCreate />)}>Create</BtnCreate>
-                                    </div>
-                                </DivFontBtn>
-                                <UlContacts className="contactsul">
-                                    {clientcont.length == 0 ? (<>
-                                        <FontH2>Voce ainda nao possui contatos</FontH2>
-                                    </>) : (<ModalEdit />)}
-                                </UlContacts>
-                            </DivContainer>
-                        </body>
-                    </DivMain>
-                </>) : (<Navigate to="/" replace />)}
+                        </DivFontBtn>
+                        <UlContacts className="contactsul">
+                            {clientcont.length == 0 ? (<>
+                                <FontH2>Voce ainda nao possui contatos</FontH2>
+                            </>) : (<ModalEdit />)}
+                        </UlContacts>
+                    </DivContainer>
+                </body>
+            </DivMain>
         </>
     )
 };
